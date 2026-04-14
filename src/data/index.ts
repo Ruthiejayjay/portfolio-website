c1574b1a3477cbbf272fadbc73cc2790a9dc9c31
Ruthiejayjay <jamesruth449@gmail.com>
2026-04-14 19:16:21 +0100

    first commit

---
+++ src/data/index.ts
@@ -0,0 +1,56 @@
+import { ExperienceItem, TechItem, NavLink } from "@/types";
+
+export const navLinks: NavLink[] = [
+  { label: "Home", href: "#home" },
+  { label: "About", href: "#about" },
+  { label: "Contact me", href: "#contact" },
+];
+
+export const experienceItems: ExperienceItem[] = [
+  {
+    period: "2023 — 2024",
+    company: "INTERNET BRAND",
+    role: "Intern at HCM Team, The Car Connection",
+    type: "Remote",
+    bullets: [
+      "Developed and maintained web applications using Zend Framework and JavaScript. The Car Connection.",
+      "Utilized Git for version control and collaboration with team members.",
+      "Managed project tasks and workflows using Jira.",
+    ],
+  },
+  {
+    period: "2023 — 2024",
+    company: "INTERNET BRAND",
+    role: "Intern at HCM Team, The Car Connection",
+    type: "Remote",
+    bullets: [
+      "Developed and maintained web applications using Zend Framework and JavaScript. The Car Connection.",
+      "Utilized Git for version control and collaboration with team members.",
+      "Managed project tasks and workflows using Jira.",
+    ],
+  },
+  {
+    period: "2023 — 2024",
+    company: "INTERNET BRAND",
+    role: "Intern at HCM Team, The Car Connection",
+    type: "Remote",
+    bullets: [
+      "Developed and maintained web applications using Zend Framework and JavaScript. The Car Connection.",
+      "Utilized Git for version control and collaboration with team members.",
+      "Managed project tasks and workflows using Jira.",
+    ],
+  },
+];
+
+export const techStack: TechItem[] = [
+  { line: 1, name: "laravel", color: "#ff4433" },
+  { line: 2, name: "react.js", color: "#61dafb" },
+  { line: 3, name: "vue />" },
+  { line: 4, name: "next.js", color: "#ffffff" },
+  { line: 5, name: "node.js", color: "#4ade80" },
+  { line: 6, name: "tailwind //", color: "#38bdf8" },
+  { line: 7, name: "GraphQL" },
+  { line: 8, name: "shadcn/ui" },
+  { line: 9, name: "...and many more //" },
+  { line: 10, name: "tailwind" },
+];
